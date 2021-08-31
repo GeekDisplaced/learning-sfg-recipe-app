20a77071662205cb9c0bfefa9af282096aaf089a
GeekDisplaced <geek.displaced@icloud.com>
2021-08-31 16:43:37 +0100

    Addition of the Argument Capture assertion approach

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,18 +1,23 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
-    // Code in Test
+
     IndexController indexController;
 
     @Mock
@@ -31,10 +36,38 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() {
 
+    // GIVEN...
+        // mock data fixtures
+        Set<Recipe> recipes = new HashSet<>();
+
+        Recipe r1 = new Recipe();
+        Recipe r2 = new Recipe();
+        Recipe r3 = new Recipe();
+
+        r1.setId(1L);
+        r2.setId(2L);
+        r3.setId(3L);
+
+        recipes.add(r1);
+        recipes.add(r2);
+        recipes.add(r3);
+
+        // mock behaviour
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+    // WHEN...
+        // code under test
         String viewName = indexController.getIndexPage(model);
 
+    // THEN...
+        // test assertions
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(3, setInController.size());
     }
 }
